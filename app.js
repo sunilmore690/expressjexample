34164d1878508b248df595e60569c2daff44ecaa
Sunil More <sunilmore690@gmail.com>
2018-08-22 17:31:21 +0530

    res changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 let app = express();
 app.get('/',(req,res,next)=>{
-    res.send('Welcome to Express App')
+    res.send('Welcome to Express App - Version1')
 })
 app.listen(3000,()=>{
     console.log('Server listening on 3000')
