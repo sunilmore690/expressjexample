d94f210e0e218b4b935b1b66b282bd3543fed125
Sunil More <sunilmore690@gmail.com>
2019-01-14 22:06:04 +0530

    add jenkins file

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ let app = express();
 app.get('/',(req,res,next)=>{
     res.send('Welcome to Express App - Version1')
 })
+app.get('/admin',(req,res,next)=>{
+    res.send('Welcome to Express App - Admin')
+})
 app.listen(3000,()=>{
     console.log('Server listening on 3000')
 })
